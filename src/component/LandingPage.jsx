60da634a5b40b29a877584d91d253c1f30b19844
kinsoo <sabistore12@gmail.com>
2021-01-21 05:56:58 -0800

    initial commit to repo

---
+++ src/component/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LadingPage = ()=>{
+    return(
+        <>
+        <div style={{width: '40%', height:'100vh'}}>
+
+        </div>
+        </>
+    )
+}
+
+
+export default LadingPage;
