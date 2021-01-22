992d0cfe20aa44ca221d5dc40ba6bdff2abcb37e
kinsoo <sabistore12@gmail.com>
2021-01-22 02:51:15 -0800

    icons turned to xl

--- src/component/LeftComponent.jsx
+++ src/component/LeftComponent.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-const element = <FontAwesomeIcon icon={faCoffee} />
+const element = <FontAwesomeIcon icon={faCoffee} size="lg"/>
 
 const LeftComponent = ()=>{
     return(
