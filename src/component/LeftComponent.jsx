5c2132f6617745873e986266166ed760a116731b
kinsoo <sabistore12@gmail.com>
2021-01-22 03:19:27 -0800

    styling for left component

--- src/component/LeftComponent.jsx
+++ src/component/LeftComponent.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
-const element = <FontAwesomeIcon icon={faCoffee} />
 
 const LeftComponent = ()=>{
     return(
         <div className="leftComponent">
-            <div>
-            helo world
-
-            {element}
+            <div>      
+                <FontAwesomeIcon icon={faTwitter} size="2x"/>
+                <span style={{width: 150, display: 'flex', flexDirection:'row', justifyContent:'space-between', marginTop: 30,}}>
+                    <FontAwesomeIcon icon={faHome} size="2x"/>
+                    <p>hello world</p>
+                </span>            
             </div>
         
         </div>
